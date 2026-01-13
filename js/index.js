0c6510ed5d12c5a757cd6074c3a4eefb934a3b3f
𝓟aͥtriͣcͫ𝒌 <patrickajm@gmail.com>
2026-01-12 23:56:42 -0300

    up

--- js/index.js
+++ js/index.js
@@ -93,8 +93,7 @@ $(function(){
       gsap.to('#navigation-content',0,{display:"none",delay:.7});
       gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
   gsap.to('#header',0,{display:"none"});
-gs    e.preventDefault();ap.to('#projects',0,{display:"none"})e){
-  
+gsap.to('#projects',0,{display:"none"});
 gsap.to('#portfolio',0,{display:"none"});
    gsap.to('#breaker',0,{display:"block"});
    gsap.to('#breaker-two',0,{display:"block",delay:.1});
@@ -103,10 +102,6 @@ gsap.to('#contact',0,{display:"none"});
    gsap.to('#breaker-two',0,{display:"none",delay:2});
    gsap.to('#about',0,{display:"block",delay:.7});
    gsap.to('#navigation-content',0,{display:'flex',delay:2});
-      // Redirecionar após animação
-    setTimeout(function() {
-        window.location.href = 'about.html';
-    }, 700); // Sincronizado com delay da animação
  })
  $('#contact-link').on('click',function(){
    gsap.to('#navigation-content',0,{display:"none",delay:.7});
@@ -140,8 +135,7 @@ $('#projects-link').on('click',function(){
   gsap.to('#navigation-content',0,{display:"none",delay:.7});
   gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
 gsap.to('#header',0,{display:"none"});
-gs    e.preventDefault();ap.to('#about',0,{display:"none"})e){
-  
+gsap.to('#about',0,{display:"none"});
 gsap.to('#portfolio',0,{display:"none"});
 gsap.to('#contact',0,{display:"none"});
 gsap.to('#breaker',0,{display:"block"});
@@ -150,10 +144,6 @@ gsap.to('#breaker',0,{display:"none",delay:2});
 gsap.to('#breaker-two',0,{display:"none",delay:2});
 gsap.to('#projects',0,{display:"block",delay:.7});
 gsap.to('#navigation-content',0,{display:'flex',delay:2});
-      // Redirecionar após animação
-    setTimeout(function() {
-        window.location.href = 'projects.html';
-    }, 700);
 })
 $('#home-link').on('click',function(){
   gsap.to('#navigation-content',0,{display:"none",delay:.7});
