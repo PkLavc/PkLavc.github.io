0d3d5e5578398fa8a5dedd4f1816f53be1538622
𝓟aͥtriͣcͫ𝒌 <patrickajm@gmail.com>
2026-01-12 23:48:48 -0300

    Corrigir navegação SPA - remover redirecionamentos incorretos
    - Removidos os blocos de window.location.href que adicionei incorretamente
    - O site é uma SPA, não precisa de redirecionamento real para about/projects
    - Mantém apenas e.preventDefault() para evitar navegação padrão do href
    - Corrige problema de about.html ficar presa na tela preta de loading
    - Agora funciona corretamente para TODAS as transições (index↔about↔projects)

--- js/index.js
+++ js/index.js
@@ -150,14 +150,6 @@ gsap.to('#breaker',0,{display:"none",delay:2});
 gsap.to('#breaker-two',0,{display:"none",delay:2});
 gsap.to('#projects',0,{display:"block",delay:.7});
 gsap.to('#navigation-content',0,{display:'flex',delay:2});
-      // Redirecionar após animação
-    setTimeout(function() {
-        window.location.href = 'projects.html';
-    }, 700);
-})
-$('#home-link').on('click',function(){
-  gsap.to('#navigation-content',0,{display:"none",delay:.7});
-  gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
 gsap.to('#header',0,{display:"none"});
 gsap.to('#about',0,{display:"none"});
 gsap.to('#portfolio',0,{display:"none"});
