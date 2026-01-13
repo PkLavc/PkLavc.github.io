9207c657b4ca4b4e81186b426e42d85f99973b65
𝓟aͥtriͣcͫ𝒌 <patrickajm@gmail.com>
2026-01-12 23:38:55 -0300

    Corrigir bug da barra vermelha e glitches visuais na transição
    - Adiciona e.preventDefault() nos links about e projects para evitar navegação dupla
    - Implementa setTimeout com window.location.href para navegação controlada após animação
    - Resolve problema da barra vermelha aparecer duas vezes
    - Elimina glitches visuais durante transições de página

--- js/index.js
+++ js/index.js
@@ -93,7 +93,8 @@ $(function(){
       gsap.to('#navigation-content',0,{display:"none",delay:.7});
       gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
   gsap.to('#header',0,{display:"none"});
-gsap.to('#projects',0,{display:"none"});
+gs    e.preventDefault();ap.to('#projects',0,{display:"none"})e){
+  
 gsap.to('#portfolio',0,{display:"none"});
    gsap.to('#breaker',0,{display:"block"});
    gsap.to('#breaker-two',0,{display:"block",delay:.1});
@@ -102,6 +103,10 @@ gsap.to('#contact',0,{display:"none"});
    gsap.to('#breaker-two',0,{display:"none",delay:2});
    gsap.to('#about',0,{display:"block",delay:.7});
    gsap.to('#navigation-content',0,{display:'flex',delay:2});
+      // Redirecionar após animação
+    setTimeout(function() {
+        window.location.href = 'about.html';
+    }, 700); // Sincronizado com delay da animação
  })
  $('#contact-link').on('click',function(){
    gsap.to('#navigation-content',0,{display:"none",delay:.7});
@@ -135,7 +140,8 @@ $('#projects-link').on('click',function(){
   gsap.to('#navigation-content',0,{display:"none",delay:.7});
   gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
 gsap.to('#header',0,{display:"none"});
-gsap.to('#about',0,{display:"none"});
+gs    e.preventDefault();ap.to('#about',0,{display:"none"})e){
+  
 gsap.to('#portfolio',0,{display:"none"});
 gsap.to('#contact',0,{display:"none"});
 gsap.to('#breaker',0,{display:"block"});
@@ -144,6 +150,10 @@ gsap.to('#breaker',0,{display:"none",delay:2});
 gsap.to('#breaker-two',0,{display:"none",delay:2});
 gsap.to('#projects',0,{display:"block",delay:.7});
 gsap.to('#navigation-content',0,{display:'flex',delay:2});
+      // Redirecionar após animação
+    setTimeout(function() {
+        window.location.href = 'projects.html';
+    }, 700);
 })
 $('#home-link').on('click',function(){
   gsap.to('#navigation-content',0,{display:"none",delay:.7});
